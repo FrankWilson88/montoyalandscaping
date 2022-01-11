9fa49779910523e31fbd2f853d1b5b63e128478d
FrankWilson88 <armyglass@hotmail.com>
2022-01-11 13:02:48 -0500

    Fixed Bugs

--- web/urls.py
+++ web/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('accounts/logout', include('django.contrib.auth.urls')),
 ]
