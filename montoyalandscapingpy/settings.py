e6520fe45d910a82ebdb300868ca42731e1948ff
FrankWilson88 <armyglass@hotmail.com>
2022-01-11 14:59:16 -0500

    Fixed Bugs

--- montoyalandscapingpy/settings.py
+++ montoyalandscapingpy/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 LOGOUT_REDIRECT_URL = '/index/'
 
 # Quick-start development settings - unsuitable for production
@@ -125,11 +125,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'web/static/')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'web/static/media')
 MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
