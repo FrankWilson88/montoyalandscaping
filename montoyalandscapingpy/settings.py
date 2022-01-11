f1945f052e9615e6e9398f4f3f750119d454b9f5
FrankWilson88 <armyglass@hotmail.com>
2022-01-11 12:36:41 -0500

    Fixed Bugs

--- montoyalandscapingpy/settings.py
+++ montoyalandscapingpy/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'web/static/')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'web/static/media')
 MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
